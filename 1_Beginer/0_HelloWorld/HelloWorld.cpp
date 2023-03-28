a68561eb6affff2b446c49a36b7357c25134969a
chipgentoo <chip.gentoo@gmail.com>
2023-03-28 20:23:54 +0300

    Hellow world

--- 1_Beginer/0_HelloWorld/HelloWorld.cpp
+++ 1_Beginer/0_HelloWorld/HelloWorld.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : HelloWorld.cpp
-// Author      : Golovchenko Alexey
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 using namespace std;
 
