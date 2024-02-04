fe23bfe93dd3d745e5f521ae5e238165dde0c719
chipgentoo <chip.gentoo@gmail.com>
2024-02-04 21:23:51 +0300

    Игрушечная история (кубики)

--- Lesson_8/3_toy_story.cpp
+++ Lesson_8/3_toy_story.cpp
@@ -3,12 +3,13 @@
 
 int main()
 {
-    float width;  // ш
-    float height; // в
-    float length; // д
+    float width  = 0; // ш
+    float height = 0; // в
+    float length = 0; // д
 
-    const int sizeSide = 5; // размер стороны кубика
-    int sizeBigCube;        // размер складываемого кобика
+    const int sizeSide = 5;         // размер стороны кубика в мм.
+    const int minSizeBigCube = 2;   // мин. размер сборного куба в исх. кубиках
+    int sizeBigCube = 0;            // размер сборного куба в исх. кубиках
 
     int numCubW;  // по ширине
     int numCubH;  // по высоте
@@ -19,30 +20,29 @@ int main()
     std::cout << "Введите размеры бруска (ширина высота длина): ";
     std::cin >> width >> height >> length;
 
-    // если одна из сторон доски < сторны кубика
-    if (width < (float)sizeSide || height < (float)sizeSide || length < (float)sizeSide)
+    // если хотя бы одна из сторон бруска меньше сторны кубика
+    if (width < sizeSide || height < sizeSide || length < sizeSide)
     {
         std::cout << "Из такого бруска кубиков не получится. Минимальный размер 5х5х5.\n ";
         return 1;
     }
-    
+
     numCubW = width / sizeSide;
     numCubH = height / sizeSide;
     numCubL = length / sizeSide;
 
-    numberOfCubes = numCubW * numCubH * numCubL;
+    numberOfCubes = numCubW * numCubH * numCubL;    // общ. кол-во кубиков
+    sizeBigCube = cbrt(numberOfCubes);              // размер стороны сборного куба
 
     std::cout << "Общее кол-во кубиков из этого бруска: " << numberOfCubes << "\n";
-    if (cbrt(numberOfCubes) < 2)
+    if (sizeBigCube < 2) // меньше мимального собираемого куба 2х2х2
     {
         std::cout << "Из " << numberOfCubes << " кубиков набора не получится.\n";
         return 1;
     }
-    
-    sizeBigCube = cbrt(numberOfCubes);
-
+ 
     std::cout << "Размер набора: " << sizeBigCube << "x" << sizeBigCube << "x" << sizeBigCube << "\n";
-    std::cout << "В наборе " << std::pow(sizeBigCube,3) << " кубиков\n";
+    std::cout << "В наборе " << std::pow( sizeBigCube, 3 ) << " кубиков\n";
 
     return 0;
 }
