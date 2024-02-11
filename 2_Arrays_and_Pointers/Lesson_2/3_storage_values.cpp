05e815fb6a885c97984b83db1fb9468d014ce4c7
chipgentoo <chip.gentoo@gmail.com>
2024-02-11 14:08:09 +0300

    Хранилище значений

--- 2_Arrays_and_Pointers/Lesson_2/3_storage_values.cpp
+++ 2_Arrays_and_Pointers/Lesson_2/3_storage_values.cpp
@@ -1,29 +1,52 @@
 #include <iostream>
 #include <vector>
 
+std::vector<int> remove_firts(std::vector<int> vec)
+{
+    std::vector<int> vecTemp(vec.size());
+    for (int i = 0; i < vec.size()-1; i++)
+    {
+        vecTemp[i] = vec[i+1]; // смещаем значения влево: 0 <- 1; 1 <- 2; 2 <- 3 и т.д.
+    }
+    vecTemp[vec.size()-1] = NULL; // крайний правый
+    return vecTemp;
+}
+
 int main()
 {
     std::vector<int> db(20);
     int inNumber;
     int count = 0;
 
-    while (count < 20)
-    {
-        std::cout << "Input number: ";
-        std::cin >> inNumber;
+    std::cout << "Input number (-1: show, -2: exit): ";
+    std::cin >> inNumber;
 
+    while (inNumber != -2)
+    {
         if (inNumber != -1)
         {
-            db[count] = inNumber;
+            if (count >= db.size())
+            {
+                db = remove_firts(db);
+                db[db.size()-1] = inNumber;
+            }
+            else
+            {
+                db[count] = inNumber;
+            }
             count++;
         }
         else
         {
+            if (count >= db.size()) count = db.size();
             for (int i = 0; i < count; i++)
             {
-                std::cout << " " << db[i] << " ";
+                std::cout << db[i] << " ";
             }
             std::cout << std::endl;
         }
+
+        std::cout << "Input number: ";
+        std::cin >> inNumber;
     }
 }
