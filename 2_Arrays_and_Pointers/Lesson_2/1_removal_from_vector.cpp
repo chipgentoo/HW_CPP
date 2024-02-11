44327f1b381a96046ab85f80d5f156e52ea0880b
chipgentoo <chip.gentoo@gmail.com>
2024-02-11 12:16:15 +0300

    Удаление из вектора

--- 2_Arrays_and_Pointers/Lesson_2/1_removal_from_vector.cpp
+++ 2_Arrays_and_Pointers/Lesson_2/1_removal_from_vector.cpp
@@ -9,12 +9,17 @@ int main()
 
     std::vector<int> intVector(vecSize);
 
+    std::cout << "Input numbers: ";
     for (int i = 0; i < vecSize; i++)
     {
         std::cin >> intVector[i];
     }
 
-    // Вывод исходного
+    int numDel;
+    std::cout << "Input number to delete: ";
+    std::cin >> numDel;
+
+    // Вывод исходного вектора
     std::cout << "Source vector:";
     for (int i = 0; i < vecSize; i++)
     {
@@ -22,8 +27,23 @@ int main()
     }
     std::cout << std::endl;
 
+    // заполнение результирующего вектора
+    std::vector<int> resVector;
+    for (int i = 0; i < intVector.size(); i++)
+    {
+        if (intVector[i] != numDel)
+        {
+            resVector.push_back(intVector[i]);
+        }
+        continue;
+    }
 
-    
-    
+    // вывод результирующего вектора
+    std::cout << "Result vector:";
+    for (int i = 0; i < resVector.size(); i++)
+    {
+        std::cout << " " << resVector[i] << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
