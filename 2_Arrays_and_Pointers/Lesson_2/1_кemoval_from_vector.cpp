9d1de463830798e742544c2bb4a1af5d1682792a
chipgentoo <chip.gentoo@gmail.com>
2024-02-11 11:39:10 +0300

    Удаление из вектора

---
+++ 2_Arrays_and_Pointers/Lesson_2/1_кemoval_from_vector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int vecSize;
+    std::cout << "Enter number of value from Vector: ";
+    std::cin >> vecSize;
+
+    std::vector<int> intVector(vecSize);
+
+    
+    return 0;
+}
